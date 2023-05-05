6769249987e11a34a778bdab51c7ba4cd5c3c789
rinel <rineliniguezsosa@gmail.com>
2023-05-05 16:37:08 -0600

    agregando el comentario PROPS

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <h1>BASIC STYLED COMPONENT</h1>
       <button>Button</button>
       <Buttonstyled>Button</Buttonstyled>
+      {/* PROPS */}
       <Buttonstyled variant="outlined">Button With Props</Buttonstyled> 
       {/* PROPS */}
 
