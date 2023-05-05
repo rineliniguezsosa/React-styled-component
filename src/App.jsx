657554913cd888575524be9ab5a6ee56ece17a9a
rinel <rineliniguezsosa@gmail.com>
2023-05-05 14:55:41 -0600

    creando una copia de Buttonstyled y pasando la prop variant igual a outlined

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
       <h1>BASIC STYLED COMPONENT</h1>
       <button>Button</button>
       <Buttonstyled>Button</Buttonstyled>
+      <Buttonstyled variant="outlined">Button With Props</Buttonstyled> 
+      {/* PROPS */}
     </>
   )
 }
