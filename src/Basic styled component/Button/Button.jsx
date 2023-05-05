7a775ae00a4c8caa01a275a94319f3e9c2b9aca3
rinel <rineliniguezsosa@gmail.com>
2023-05-05 15:03:01 -0600

    agregando las props y condicionando el color si la prop.variant es igual a outlined entonces el color del texto sera amarillo y si no pues palevioletred

--- src/Basic styled component/Button/Button.jsx
+++ src/Basic styled component/Button/Button.jsx
@@ -4,7 +4,7 @@ export const Buttonstyled = styled.button`
   background: transparent;
   border-radius: 3px;
   border: 2px solid palevioletred;
-  color: palevioletred;
+  color: ${props => props.variant === 'outlined' ? 'yellow':'palevioletred'};
   margin: 0 1em;
   padding: 0.25em 1em;
 `
